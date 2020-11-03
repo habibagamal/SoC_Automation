86e44e1165c72e7a8c03e274722f0b950e8e6e45
habibagamal <habibabassem@aucegypt.edu>
2020-11-03 20:04:33 +0200

    unnecessary lines removed

--- masters/N5/build/sw/base_addr.h
+++ masters/N5/build/sw/base_addr.h
@@ -7,41 +7,7 @@
 #define     I2C_BASE_ADDR_0       0x40000000
 #define     UART_BASE_ADDR_0       0x40100000
 #define     UART_BASE_ADDR_1       0x40200000
-	
 #define     APB_SPI_BASE_ADDR_0       0x40300000
 #define     PWM_BASE_ADDR_0       0x40400000
-// #define     PWM_BASE_ADDR_1       0x40600000
-// #define     ADC_BASE_ADDR_0       0x40700000
-// #define     ADC_BASE_ADDR_1       0x40800000
-	
-// #define     DAC_BASE_ADDR_0       0x40900000
-	
-// #define     COMPARATOR_BASE_ADDR_0     0x40a00000
-	
-// #define     BG_BASE_ADDR_0       0x40b00000
-	
-// #define     CLK_CTRL_BASE_ADDR_0       0x50000000
-	
-// #define     AHB_SPI_BASE_ADDR_1       0x70000000
-	
 #define     AHB_GPIO_BASE_ADDR_0      0x48000000
-	
-
-
-// #define     APB_GPIO_BASE_ADDR_0		INVALID_ADDR
-
-
-
-
-
-
-
-
 #define     TIMER_BASE_ADDR_0      		0x40500000
-
-
-
-
-
-
-// #define     DBGIO_BASE_ADDR_0     0x24000000
